7fb746a4bc6d6af7fa651d70ebfc879480965c6e
David Wamai <davwamai@gmail.com>
2022-12-12 18:43:41 -0500

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,34 @@
 fn main() {
     let inputs = [1.0, 2.0, 3.0, 2.5];
 
-    let weights1 = [0.2, 0.8, -0.5, 1.0];
-    let weights2 = [0.5, -0.91, 0.26, -0.5];
-    let weights3 = [-0.26, -0.27, 0.17, 0.87];
 
-    let bias1 = 2.0;
-    let bias2 = 3.0;
-    let bias3 = 0.5;
+    let weights = [[0.2, 0.8, -0.5, 1.0],
+                [0.5, -0.91, 0.26, -0.5],
+                [-0.26, -0.27, 0.17, 0.87]];
 
+   
+    let biases = [2.0, 3.0, 0.5];
 
-    let output = [inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] + inputs[3]*weights1[3] + bias1,
-                inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3] + bias2,
-                inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3] + bias3];
+    let mut layer_outputs = Vec::new();
 
+    let iter = weights.iter().zip(biases.iter());
 
-    for n in output {
-        println!("{}", n);
+    for it in iter{
+        let (neuron_weight, neuron_bias) = it;
+        let mut neuron_output = 0.0;
+        let inp_and_weights = inputs.iter().zip(neuron_weight.iter());
+        for _inpw_iter in inp_and_weights{
+            let (inp, wt) = _inpw_iter;
+            neuron_output += inp*wt;    
+        }
+        neuron_output += neuron_bias;
+        layer_outputs.push(neuron_output);
     }
+
+    // for n in layer_outputs{
+    //     println!("{}", n);
+    // }
+        println!("{:#?}",layer_outputs);
+
     
 }
